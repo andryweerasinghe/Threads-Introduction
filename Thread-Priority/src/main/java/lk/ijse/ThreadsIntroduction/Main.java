d78e9d19111ad44b4fccce1ce6c2921fea0607a6
andryweerasinghe <andryweerasinghe@gmail.com>
2024-04-05 12:13:23 +0530

    Synchronized Keyword

--- Thread-Priority/src/main/java/lk/ijse/ThreadsIntroduction/Main.java
+++ Thread-Priority/src/main/java/lk/ijse/ThreadsIntroduction/Main.java
@@ -5,14 +5,14 @@ public class Main {
 
         Thread thread1 = new Thread(() -> {
             for (int i = 1; i <= 5; i++) {
-                System.out.println("One");
+                System.out.println("One " + Thread.currentThread().getName()); //Thread.currentThread() method allows to find out which thread runs
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
-        });
+        }, "uba khd");
 
         Thread thread2 = new Thread(() -> {
             for (int i = 1; i <= 5; i++) {
@@ -23,17 +23,22 @@ public class Main {
                     throw new RuntimeException(e);
                 }
             }
-        });
+        },"man mehe");
 
         thread1.start();
-        thread1.setName("Thread One");
+//        thread1.setName("Thread One");
         try {
             Thread.sleep(10);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Thread Name : " + thread1.getName());
+//        System.out.println("Thread Name : " + thread1.getName());
+//        System.out.println("Thread Name : " + thread2.getName()) ;
+//        System.out.println("Thread Priority : " + thread1.getPriority());
+//        System.out.println("Thread Priority : " + thread2.getPriority());
         thread2.start();
+        thread1.setPriority(Thread.MAX_PRIORITY);
+        thread2.setPriority(Thread.MIN_PRIORITY);
 
         thread1.join();
         thread2.join();
